5847b74841cdd1635f8dd6b9e6a3990aa9c044c0
Eva Machova <evca.machova@gmail.com>
2023-10-19 23:03:14 +0200

    Add react select

--- frontend/src/TravelListForm/index.tsx
+++ frontend/src/TravelListForm/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
 import tentImg from './img/tent.jpg';
 import hotelImg from './img/hotel.jpg';
 import './style.css';
+import { useEffect, useState } from 'react';
+import Select from 'react-select';
+import { Gear, GearList, Item, fetchGearOptions } from '../api/gear';
+
+// interface FormValues {
+//   stayLength: number;
+//   tripType: 'tent' | 'hotel';
+// }
 
 const PackingGuide = (): JSX.Element => {
+  const [gear, setGear] = useState<GearList | 'loading'>('loading');
+  useEffect(() => {
+    const fetchGearData = async (): Promise<void> => {
+      const response = await fetchGearOptions();
+      setGear(response);
+    };
+    void fetchGearData();
+  }, []);
+
+  console.log(gear);
+
   return (
     <>
-      <h1 className="text-center mb-8">
-        Sbal se na další výlet rychle a bez stresu!
-      </h1>
-      <form className="flex flex-col gap-8 align-center packingForm">
+      <h1 className="mb-8">Sbal se na další výlet rychle a bez stresu!</h1>
+      <form className="gap-8 packingForm">
+        <h2>1. Základní info</h2>
         <label>
-          Délka pobytu:
+          Na kolik dní:
           <input
-            className="p-1 ... bg-slate-300 rounded-md border-zinc-500"
+            className="p-1 bg-slate-300 rounded-md border-zinc-500"
             type="number"
             name="stayLength"
           />
@@ -25,12 +42,12 @@ const PackingGuide = (): JSX.Element => {
               <input
                 className="input-hidden"
                 type="radio"
-                name="tentOption"
-                id="tentOption"
+                name="tent"
+                id="tent"
               />
-              <label htmlFor="tentOption">
+              <label htmlFor="tent">
                 <img
-                  className="h-28 cursor-pointer"
+                  className="w-80 cursor-pointer"
                   src={tentImg}
                   alt="camping"
                 />
@@ -40,12 +57,12 @@ const PackingGuide = (): JSX.Element => {
               <input
                 className="input-hidden"
                 type="radio"
-                name="tentOption"
-                id="hotelOption"
+                name="hotel"
+                id="hotel"
               />
-              <label htmlFor="hotelOption">
+              <label htmlFor="hotel">
                 <img
-                  className="h-28 cursor-pointer"
+                  className="w-80 cursor-pointer"
                   src={hotelImg}
                   alt="hotel"
                 />
@@ -53,7 +70,21 @@ const PackingGuide = (): JSX.Element => {
             </div>
           </div>
         </label>
-
+        {gear === 'loading' ? (
+          <div>Loading...</div>
+        ) : (
+          gear.map((group: Gear) => (
+            <label>
+              {group.group}
+              <Select
+                options={group.items.map((item: Item) => ({
+                  value: item.name,
+                  label: item.name,
+                }))}
+              ></Select>
+            </label>
+          ))
+        )}
         <button
           className="h-10 px-6 font-semibold w-48 rounded-md bg-primary text-white"
           type="submit"
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-
 const Header = (): JSX.Element => {
   return (
     <div className="flex justify-between align-middle m-6">
       <div>Gear Up</div>
-      <button>Přihlásit se</button>
     </div>
   );
 };
